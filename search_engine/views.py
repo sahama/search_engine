3929f644358d011ac86fc67c54db80cbdd71e382
me <s.h.mahdavi@chmail.ir>
2017-07-16 18:47:47 +0430

    modify search page template

--- search_engine/views.py
+++ search_engine/views.py
@@ -1,6 +1,15 @@
 from pyramid.view import view_config
+import pyramid_bowerstatic
+import os
+
+components = pyramid_bowerstatic.create_components('search_engine',
+                                                   os.path.join(os.path.dirname(__file__), 'static',
+                                                                'bower_components'))
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.jinja2')
+@view_config(route_name='home', renderer='templates/home.jinja2')
 def my_view(request):
+    request.include(components, 'bootstrap')
+    request.include(components, 'jquery')
+
     return {'project': 'search_engine'}
--- setup.py
+++ setup.py
@@ -3,10 +3,8 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.txt')) as f:
+with open(os.path.join(here, 'README.rst')) as f:
     README = f.read()
-with open(os.path.join(here, 'CHANGES.txt')) as f:
-    CHANGES = f.read()
 
 requires = [
     'pyramid',
@@ -24,7 +22,7 @@ tests_require = [
 setup(name='search_engine',
       version='0.0',
       description='search_engine',
-      long_description=README + '\n\n' + CHANGES,
+      long_description=README,
       classifiers=[
           "Programming Language :: Python",
           "Framework :: Pyramid",
